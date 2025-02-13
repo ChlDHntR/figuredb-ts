9c110efb64e0f20f615a3fa1624d63a10d98290f
Thanh <cringeclone@gmail.com>
2025-02-13 18:05:56 +0900

    completed add trade function

--- src/component/TradingPage.tsx
+++ src/component/TradingPage.tsx
@@ -5,28 +5,33 @@ import { AnyTxtRecord } from 'dns'
 import AddTradeCard from './AddTradeCard'
 
 export default function TradingPage({ data }: any) {
-  const tradeData = useRef<any>(null)
+  const tradeData = useRef<any>([])
   const [isLoading, setIsLoading] = useState(true)
   const [addTrade, setAddTrade] = useState(false)
+  const [frieren, setFrieren] = useState({})
+  const [reversedData, setReversedData] = useState<any>([])
 
   useEffect(() => {
     server.get('trades').then((res) => {
       tradeData.current = res.data
       setIsLoading(false)
+      let dummyData = [...tradeData.current!].reverse()
+      setReversedData(dummyData)
     })
-  }, [])
+  }, [frieren])
 
-  if (isLoading) return <div className='comment_section box'>IS LOADING</div>
+  if (isLoading) return <div className='comment_section box'>IS LOADING...</div>
   return (
     <div className='trading-page'>
       <div className='wide'>
         {addTrade ?
-          <AddTradeCard figureData={data} />
+          <AddTradeCard figureData={data} setAddTrade={setAddTrade} forceReRender={() => setFrieren({})}/>
         : <div className="add-trade-btn box" style={{marginBottom: '20px'}} onClick={() => setAddTrade(true)}>
           <h3>Add trade</h3>
         </div>
         }
-        {tradeData.current!.map((item: any) => (
+        {
+        reversedData.map((item: any) => (
           <TradingCard key={item.id} tradeInfo={{ ...item }} figureData={data} />
         ))}
       </div>
